3af572e2fdbfc462cc35548cacbe7aae87724455
roovies <roovies77@gmail.com>
2025-08-15 15:46:53 +0900

    feat: 포인트 충전/사용 내역 조회 기능 controller 누락 커밋

--- src/main/java/io/hhplus/tdd/point/controller/PointController.java
+++ src/main/java/io/hhplus/tdd/point/controller/PointController.java
@@ -35,7 +35,7 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return pointService.findPointHistoryByUserId(id);
     }
 
     /**
